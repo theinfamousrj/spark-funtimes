a100706d14a8a55881a171029a65fd09c9b4f7af
RJ <raymondjohnhill@gmail.com>
2023-07-03 14:56:02 -0400

    Changing my mixed case to all snake_case

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ order_by = "trip_distance DESC"
 spark = SparkSession.builder.appName("SparkFuntimes").getOrCreate()
 
 # Read in the Parquet file from the NYC Taxi Data
-parquetFile = spark.read.parquet(file_name)
+parquet_file = spark.read.parquet(file_name)
 
 # Let's also get the size of the file too, just in case
 file_size = os.path.getsize(file_name)
 
 # Parquet files can also be used to create a temporary view (like in a traditional DB) and then used in SQL statements.
-parquetFile.createOrReplaceTempView("parquetFile")
+parquet_file.createOrReplaceTempView("parquet_file")
 
 # Check the file size and if it is larger than file_size_limit (set above), we should to run an approximate
 # Current size: 47673370 bytes
 if file_size > file_size_limit:
     print("File size is too large to run exact percentile")
-    percentile_sql = "SELECT percentile_approx(trip_distance, " + str(percentile) + ") FROM parquetFile"
+    percentile_sql = "SELECT percentile_approx(trip_distance, " + str(percentile) + ") FROM parquet_file"
 else:
     print("File size is small enough to run exact percentile")
-    percentile_sql = "SELECT percentile(trip_distance, " + str(percentile) + ") FROM parquetFile"
+    percentile_sql = "SELECT percentile(trip_distance, " + str(percentile) + ") FROM parquet_file"
 
 percentile = spark.sql(percentile_sql)
 
@@ -37,6 +37,6 @@ percentile_value = percentile.collect()[0][0]
 print("90th percentile is: ", percentile_value)
 
 # Now we build the actual query to get the data we want
-data_sql = "SELECT VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_distance FROM parquetFile WHERE trip_distance > " + str(percentile_value) + " ORDER BY " + order_by
+data_sql = "SELECT VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_distance FROM parquet_file WHERE trip_distance > " + str(percentile_value) + " ORDER BY " + order_by
 data = spark.sql(data_sql)
 data.show()
